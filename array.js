bf29d190a63c80171b905e8b3ebdab94d7079893
Lokendra Singh <lokendra.06.work@gmail.com>
2025-12-01 08:27:32 +0530

    day1 array

--- array.js
+++ array.js
@@ -23,4 +23,42 @@ function reverseArr(arr) {
 
     return arr;
 }
-console.log(reverseArr(arr));
+// console.log(reverseArr(arr));
+
+
+
+//find largest element
+
+const arr2 = [45, 85, 122, 1, 6, 8];
+
+function findLargest(arr) {
+    let largest = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i]
+        }
+    }
+    return largest;
+}
+// console.log(findLargest(arr2));
+
+
+//find second largest element
+
+// const arr2 = [10, 5, 10];
+
+function findSecondLargest(arr) {
+    let largest = arr[0];
+    let secondLargest = -1;
+    let temp;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] !== largest) {
+            secondLargest = arr[i]
+        }
+    }
+    return secondLargest;
+}
+console.log(findSecondLargest(arr2));
