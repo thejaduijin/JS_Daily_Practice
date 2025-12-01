0486f7792ed7130a015fe3dbef4ff3348043253b
Lokendra Singh <lokendra.06.work@gmail.com>
2025-12-01 09:07:31 +0530

    day1 frequency of char

--- strings.js
+++ strings.js
@@ -14,3 +14,24 @@ function correctOrder(str) {
     return str.split("").sort().join("");
 }
 console.log(correctOrder(str2));
+
+
+// count frequency of char 
+const str3 = "abbcaadefhh";
+
+function countFrequency(str) {
+    let count = 1;
+    let req = {}
+
+    for (let char of str) {
+        // if (req.hasOwnProperty(char)) {
+        if (req[char]) {
+            req[char] += 1;
+        } else {
+            req[char] = count;
+        }
+    }
+    return req;
+}
+
+console.log(countFrequency(str3));
