77c90b62ad50b2ffd6c9298d3889a32e65977930
Lokendra Singh <lokendra.06.work@gmail.com>
2025-12-01 08:50:41 +0530

    day1 alphabetical order

--- strings.js
+++ strings.js
@@ -1,8 +1,16 @@
 // reverse an string
-
 const str = "abcd";
 
-function reverseStr(str){
-    return str.split("").reverse().join(""); 
+function reverseStr(str) {
+    return str.split("").reverse().join("");
 }
 console.log(reverseStr(str));
+
+
+// correct in alphabetical order
+const str2 = "baced";
+
+function correctOrder(str) {
+    return str.split("").sort().join("");
+}
+console.log(correctOrder(str2));
